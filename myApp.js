5eb0df68bb8c5a9af8ca3ee482e6a25359761a5d
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-11-24 03:41:57 +0000

    Changes to be committed: 	modified:   myApp.js 	modified:   package.json

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
-
-
-
+const absolutePath = __dirname + '/views/index.html'
+const assetsAbsolutePath = __dirname + '/public'
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath)
+})
+app.use('/public', express.static(assetsAbsolutePath))
 
 
 
