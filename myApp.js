a8729695d185b101ebbf811f63e178f98abb7e0c
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-11-25 09:09:41 +0000

    Changes to be committed: 	modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
-let express = require('express');
 const res = require('express/lib/response');
+let express = require('express');
+let bodyParser = require('body-parser')
+
+require('dotenv').config()
 let app = express();
 const absolutePath = __dirname + '/views/index.html'
 const assetsAbsolutePath = __dirname + '/public'
@@ -9,16 +12,46 @@ app.get('/', (req, res) => {
 app.use('/public', express.static(assetsAbsolutePath))
 
 
+// app.get('/json', (req, res) => {
+//     if(process.env.MESSAGE_STYLE === 'uppercase'){
+//         res.json({"message": "HELLO JSON"})
+//     } else {
+//         res.json({"message": "Hello json"})
+//     }
+// })
+
+// function mware(req, res, next){
+//     console.log(`${req.method} ${req.path} - ${req.ip}`)
+//     next()
+// }
+
+// app.use(mware)
+
+// function mware(req, res, next){
+//     req.time = new Date().toString()
+//     next()
+// }
+
+// app.get('/now',  (req, res, next) => {
+//     req.time = new Date().toString()
+//     next()
+// }, (req, res) => {
+//     console.log(req.time)
+//     res.json({"time": `${req.time}`})
+// })
+
+// app.get('/:word/echo', (req, res) => {
+//     res.json({"echo":`${req.params.word}`})
+// })
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 
-
-
-
-
-
-
-
-
+app.route('/name').get((req, res) => {
+    res.json({"name": `${req.query.first} ${req.query.last}`})
+}).post((req, res)=> {
+    res.json({"name": `${req.body.first} ${req.body.last}`})
+})
 
 
 
